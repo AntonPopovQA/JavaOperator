05e13c86e574af3f5eb010394653c85e02cf7050
Anton <toni-popov93@mail.ru>
2022-05-24 22:37:10 +0300

    Operator

---
+++ src/main.java
@@ -0,0 +1,14 @@
+public class main {
+    public static void main(String[] args) {
+        int invoiceAmout = 300;
+        int replenishmentAmount = 1500;
+        int bonus = replenishmentAmount / 100;
+        int grandTotal = invoiceAmout + replenishmentAmount;
+
+        if (replenishmentAmount < 1000) {
+            bonus = 0;
+        }
+        System.out.println("Итоговый бонус: " + bonus);
+        System.out.println("Итоговый счет: " + grandTotal);
+    }
+}
